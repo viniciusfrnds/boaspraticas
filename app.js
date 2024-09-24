3cf4f5289033f1ac0418149a899f50f2acd8e085
viniciusfrnds <vini.vfv36@gmail.com>
2024-09-24 10:04:11 -0300

    Adiciona componente App com mensagem de boas-vindas

---
+++ app.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div>
+        <h1>Bem-vindo ao Meu Jogo!</h1>
+        <p>Prepare-se para se divertir!</p>
+        <button onClick={() => alert('Jogo Iniciado!')}>Iniciar Jogo</button>
+      </div>
+    );
+  }
+  
